3a277e756ce59bbdc5eb05bd18be78da149c917d
MohdBasurra <mohdye96@gmail.com>
2023-01-15 00:39:52 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function yearWin(recordYear) {
+    if (recordYear.result == "W") {
+        return true;
+    }
+    return false;
+}
+function superbowlWin(records) {
+
+    if (records.find(yearWin)) {
+        return records.find(yearWin).year;
+    }
+    return undefined;
+}record.result
